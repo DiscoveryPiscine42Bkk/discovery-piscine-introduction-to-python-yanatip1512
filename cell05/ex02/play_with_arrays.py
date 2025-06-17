cdc6db56146e672e2819ecdd902e693e62c319fb
yanatip1512 <klaza2549@gmail.com>
2025-06-17 16:48:00 +0000

    52

---
+++ cell05/ex02/play_with_arrays.py
@@ -0,0 +1,12 @@
+# play_with_arrays.py
+
+def main():
+    original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+    print(original_array)
+
+    # กรองค่า >= 8 แล้วบวก 2
+    new_array = [x + 2 for x in original_array if x >= 8]
+    print(new_array)
+
+if __name__ == "__main__":
+    main()
